113806879eec180e4331590c5704a9399e541c5c
Rahul Butani <rr.butani@gmail.com>
2020-07-21 16:20:49 -0500

    52

--- src/main.rs
+++ src/main.rs
@@ -319,7 +319,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-#![recursion_limit = "1024"]
+#![recursion_limit = "16384"]
 
 #![feature(fn_traits)]
 #![feature(unboxed_closures)]
@@ -399,17 +399,58 @@ macro_rules! right_recursive {
 }
 
 
-adder_func!((a): (A));
-adder_func!((a, b): (A, B));
-adder_func!((a, b, c): (A, B, C));
-adder_func!((a, b, c, d): (A, B, C, D));
-adder_func!((a, b, c, d, e): (A, B, C, D, E));
-adder_func!((a, b, c, d, e, f): (A, B, C, D, E, F));
-adder_func!((a, b, c, d, e, f, g): (A, B, C, D, E, F, G));
-adder_func!((a, b, c, d, e, f, g, h): (A, B, C, D, E, F, G, H));
-adder_func!((a, b, c, d, e, f, g, h, i): (A, B, C, D, E, F, G, H, I));
-adder_func!((a, b, c, d, e, f, g, h, i, j): (A, B, C, D, E, F, G, H, I, J));
-adder_func!((a, b, c, d, e, f, g, h, i, j, k): (A, B, C, D, E, F, G, H, I, J, K));
+adder_func!((a):                                                                                                                                                                                    (A));
+adder_func!((a, b):                                                                                                                                                                                 (A, B));
+adder_func!((a, b, c):                                                                                                                                                                              (A, B, C));
+adder_func!((a, b, c, d):                                                                                                                                                                           (A, B, C, D));
+adder_func!((a, b, c, d, e):                                                                                                                                                                        (A, B, C, D, E));
+adder_func!((a, b, c, d, e, f):                                                                                                                                                                     (A, B, C, D, E, F));
+adder_func!((a, b, c, d, e, f, g):                                                                                                                                                                  (A, B, C, D, E, F, G));
+adder_func!((a, b, c, d, e, f, g, h):                                                                                                                                                               (A, B, C, D, E, F, G, H));
+adder_func!((a, b, c, d, e, f, g, h, i):                                                                                                                                                            (A, B, C, D, E, F, G, H, I));
+adder_func!((a, b, c, d, e, f, g, h, i, j):                                                                                                                                                         (A, B, C, D, E, F, G, H, I, J)) ;
+adder_func!((a, b, c, d, e, f, g, h, i, j, k):                                                                                                                                                      (A, B, C, D, E, F, G, H, I, J, K));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l):                                                                                                                                                   (A, B, C, D, E, F, G, H, I, J, K, L));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m):                                                                                                                                                (A, B, C, D, E, F, G, H, I, J, K, L, M));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n):                                                                                                                                             (A, B, C, D, E, F, G, H, I, J, K, L, M, N));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o):                                                                                                                                          (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p):                                                                                                                                       (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q):                                                                                                                                    (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):                                                                                                                                 (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s):                                                                                                                              (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t):                                                                                                                           (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u):                                                                                                                        (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v):                                                                                                                     (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w):                                                                                                                  (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x):                                                                                                               (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y):                                                                                                            (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z):                                                                                                         (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa):                                                                                                     (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab):                                                                                                 (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac):                                                                                             (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad):                                                                                         (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae):                                                                                     (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af):                                                                                 (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag):                                                                             (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah):                                                                         (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai):                                                                     (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj):                                                                 (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak):                                                             (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al):                                                         (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am):                                                     (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an):                                                 (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao):                                             (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap):                                         (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq):                                     (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar):                                 (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, a_s):                             (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR, AS));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, a_s, at):                         (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR, AS, AT));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, a_s, at, au):                     (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR, AS, AT, AU));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, a_s, at, au, av):                 (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR, AS, AT, AU, AV));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, a_s, at, au, av, aw):             (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR, AS, AT, AU, AV, AW));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, a_s, at, au, av, aw, ax):         (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR, AS, AT, AU, AV, AW, AX));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, a_s, at, au, av, aw, ax, ay):     (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR, AS, AT, AU, AV, AW, AX, AY));
+adder_func!((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, a_s, at, au, av, aw, ax, ay, az): (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, AC, AD, AE, AF, AG, AH, AI, AJ, AK, AL, AM, AN, AO, AP, AQ, AR, AS, AT, AU, AV, AW, AX, AY, AZ));
 
 macro_rules! units {
     ($($id:ident),*) => {$(
@@ -456,10 +497,11 @@ add_impls!{
 
 fn main() {
     let a = Adder(Zero);
-
     assert_eq!(a(Two), Two);
-
 
     let add_2048 = Adder(B2);
     assert_eq!(add_2048(B2, B4, B8, B16, B32, B64, B128, B256, B512, B1024), B2048);
+
+    let b = Adder(1);
+    assert_eq!(b(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1), 53);
 }
